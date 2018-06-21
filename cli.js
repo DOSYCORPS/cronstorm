4f39f366e3905cb91b403e5f47fb76890dbbeb7b
Cris @ DOSY <22254235+dosyago-coder-0@users.noreply.github.com>
2018-06-21 22:00:32 +0800

    ok

--- cli.js
+++ cli.js
@@ -3,5 +3,12 @@
 "use strict";
 
 {
+  const CF = require('configstore');
+  const pkg = require('./package.json');
+  const cf = new CF(pkg, {'apiKey': 'chronos'});
+  const kairoi = require('./kairoi.js');
 
+  const argv = require('yargs').argv;
+
+  console.log(argv);
 }
