1f5e14fe39cd702c876c593941c3847447841ea8
Cris @ DOSY <22254235+dosyago-coder-0@users.noreply.github.com>
2018-06-21 22:43:09 +0800

    ok

--- cli.js
+++ cli.js
@@ -7,36 +7,48 @@
   const pkg = require('./package.json');
   const cf = new CF(pkg, {'apiKey': 'chronos'});
   const kairoi = require('./kairoi.js');
+  const TIME_UNITS = ['second', 'minute', 'hour', 'week', 'month'];
+  const singularize = v => v.endsWith('s') ? v.slice(0,-1) : v;
 
   const argv = require('yargs')
-    .command(['auth', '$0'], 'the auth command', () => {}, argv => {
+    .command(['auth', '$0'], 'auth in with your API key', () => {}, argv => {
       console.log(
         'To get an API key, visit this link in your browser: https://api.pocketwatch.xyz');
     })
-    .command('* <method> <url>', 'request specifier', () => {}, argv => {
-      console.log( 'request', argv.method, argv.url );
-    });
     .command({
-      command: 'every <i> <t>',
+      command: 'every <i> <t> for <I> <T> <method> <url>',
       handler: argv => {
         argv.interval_unit_count = argv.i;
         argv.interval_unit_type = argv.t;
+        argv.duration_unit_count = argv.I;
+        argv.duration_unit_type = argv.T;
       }
     })
-    .command({
-      command: 'for <i> <t>',
-      handler: argv => {
-        argv.duration_unit_count = argv.i;
-        argv.duration_unit_type = argv.t;
-      }
+    .coerce('interval_unit_type', v => {
+      v = v + '';
+      v = v.toLowerCase();
+      v = singularize(v);
+      return v;
     })
+    .choices('interval_unit_type', TIME_UNITS);
+    .coerce('duration_unit_type', v => {
+      v = v + '';
+      v = v.toLowerCase();
+      v = singularize(v);
+      return v;
+    })
+    .choices('duration_unit_type', TIME_UNITS);
     .option('body', {
       describe: 'specify an entity request body'
     })
-    .optiona('contentType', {
+    .option('contentType', {
       describe: 'specify the MIME string to be sent as Content-Type header'
     })
-    
+    .option('apiKey', {
+      describe: 'override the saved apiKey'
+    })
+    .help()
+    .argv
 
   console.log(argv);
 }
