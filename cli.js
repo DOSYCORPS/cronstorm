2330411a74e71ed71d1af920e4156385ef654e9b
Cris @ DOSY <22254235+dosyago-coder-0@users.noreply.github.com>
2018-06-21 23:03:19 +0800

    ok

--- cli.js
+++ cli.js
@@ -6,8 +6,11 @@
   const CF = require('configstore');
   const pkg = require('./package.json');
   const cf = new CF(pkg, {'apiKey': 'chronos'});
+  const {URL} = require('url');
+
   const kairoi = require('./kairoi.js');
   const TIME_UNITS = ['second', 'minute', 'hour', 'week', 'month'];
+  const METHOD = ["GET","POST","PUT","DELETE","PATCH","HEAD"];
   const singularize = v => v.endsWith('s') ? v.slice(0,-1) : v;
 
   const argv = require('yargs')
@@ -16,28 +19,61 @@
         'To get an API key, visit this link in your browser: https://api.pocketwatch.xyz');
     })
     .command({
-      command: 'every <i> <t> for <I> <T> <method> <url>',
+      command: '$0 <method> <url> every <interval> <time> for <total> <Time>',
+      describe: 'begin making requests',
       handler: argv => {
-        argv.interval_unit_count = argv.i;
-        argv.interval_unit_type = argv.t;
-        argv.duration_unit_count = argv.I;
-        argv.duration_unit_type = argv.T;
+        argv.interval_unit_count = argv.interval;
+        argv.interval_unit_type = argv.time;
+        argv.duration_unit_count = argv.total;
+        argv.duration_unit_type = argv.Time;
       }
     })
-    .coerce('interval_unit_type', v => {
+    .command({
+      command: 'every <interval> <time> for <total> <Time> <method> <url>',
+      alias: '$0 <method> <url> every <interval> <time> for <total> <Time>',
+      describe: 'begin making requests',
+      handler: argv => {
+        argv.interval_unit_count = argv.interval;
+        argv.interval_unit_type = argv.time;
+        argv.duration_unit_count = argv.total;
+        argv.duration_unit_type = argv.Time;
+      }
+    })
+    .coerce('interval', v => {
+      v = parseInt(v);
+      if ( ! Number.isInteger(v) ) {
+        throw new Error("Integer required for interval");
+      }
+      return v;
+    })
+    .coerce('total', v => {
+      v = parseInt(v);
+      if ( ! Number.isInteger(v) ) {
+        throw new Error("Integer required for total");
+      }
+      return v;
+    })
+    .coerce('time', v => {
       v = v + '';
       v = v.toLowerCase();
       v = singularize(v);
       return v;
     })
-    .choices('interval_unit_type', TIME_UNITS);
-    .coerce('duration_unit_type', v => {
+    .choices('time', TIME_UNITS)
+    .coerce('Time', v => {
       v = v + '';
       v = v.toLowerCase();
       v = singularize(v);
       return v;
     })
-    .choices('duration_unit_type', TIME_UNITS);
+    .choices('Time', TIME_UNITS)
+    .coerce('url', v => {
+      new URL(v);
+      return v;
+    })
+    .coerce('method', v => (v+'').toUpperCase())
+    .choices('method', METHOD)
+    .example('every 1 seconds for 10 weeks post https://localhost')
     .option('body', {
       describe: 'specify an entity request body'
     })
